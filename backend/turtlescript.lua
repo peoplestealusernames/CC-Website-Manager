8c92a3c0aec8dda5769ba4ebcc959f61ad8755d9
Brett <bretthoganpr@gmail.com>
2022-08-15 01:02:23 -0400

    turtlescript: Redo

--- backend/turtlescript.lua
+++ backend/turtlescript.lua
@@ -1,11 +1,23 @@
 local ws, err = http.websocket("ws://localhost:5500")
 
-while true do
-    local msg = ws.recive()
-    local loadfnc, loaderr = load(msg)
-    if loadfnc then
-        ws.send(
-            pcall(loadfnc)
-        )
+if (not ws) then
+    return err
+end
+
+function ProccessCall(fncString)
+    local loadfnc, loaderr = load(fncString);
+    if (not loadfnc) then
+        loadfnc, loaderr = load("return " .. fncString);
     end
+    if (not loadfnc) then
+        return false, loaderr;
+    end
+    return pcall(loadfnc);
+end
+
+while true do
+    local msg = ws.receive()
+    ws.send(
+        ProccessCall(msg)
+    )
 end
