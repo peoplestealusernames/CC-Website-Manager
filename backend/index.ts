304cddffd501e133b7953690fe643cbaac1985d3
Brett <bretthoganpr@gmail.com>
2022-08-15 01:55:37 -0400

    Backend: computer indexer from header

--- backend/index.ts
+++ backend/index.ts
@@ -3,9 +3,11 @@ import express from "express";
 import { createServer } from "http";
 import { JsonDB } from "node-json-db";
 import { Config } from "node-json-db/dist/lib/JsonDBConfig";
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const blockdata = new JsonDB(new Config("./blockdata", true, true, "/"))
+
+const computers: { [id: string]: WebSocket } = {}
 
 const app = express()
 app.use(cors({ origin: true }))
@@ -19,8 +21,13 @@ app.get("/blocks", async (req, res) => {
 
 const server = createServer(app)
 const sock = new WebSocketServer({ server })
-sock.on('connection', client => {
-    console.log("connect");
+sock.on('connection', (client, req) => {
+    console.log("connect")
+    if (req.headers.type === "computer" && typeof req.headers.computerid === "string") {
+        computers[req.headers.computerid] = client
+        console.log(`Computer: ${req.headers.computerid} connected`);
+    }
+
     client.on('message', (data) => console.log(data.toString()));
     client.on("close", () => console.log("close"))
 });
