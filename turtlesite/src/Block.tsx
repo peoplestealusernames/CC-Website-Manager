d43c7e510b0c9d3bef44df115db94e866229b57e
Brett <bretthoganpr@gmail.com>
2022-08-15 22:52:16 -0400

    BlockRender: swapped to useTexture

--- turtlesite/src/Block.tsx
+++ turtlesite/src/Block.tsx
@@ -1,12 +1,13 @@
-import { Object3DNode, useFrame, useLoader, Vector3 } from "@react-three/fiber"
+import { useTexture } from "@react-three/drei"
+import { Object3DNode, useFrame, Vector3 } from "@react-three/fiber"
 import { useRef, useState } from "react"
-import { Texture, TextureLoader } from "three"
+import { Texture } from "three"
 import { typeBlock } from "./type"
 
 export function BlockRender(props: {
     block: typeBlock
 }) {
-    const colorMap = useLoader(TextureLoader, `textures/${props.block.name.split(":").splice(1).join(":")}.png`)
+    const colorMap = useTexture(`textures/${props.block.name.split(":").splice(1).join(":")}.png`)
 
     return (
         <mesh
