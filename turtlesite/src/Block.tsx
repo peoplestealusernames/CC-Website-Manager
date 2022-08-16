3099510d52f763f7d486b7eea9214e49feadc1df
Brett <bretthoganpr@gmail.com>
2022-08-15 23:03:48 -0400

    Frontend: Texturemap to prevent crash on bad load

--- turtlesite/src/Block.tsx
+++ turtlesite/src/Block.tsx
@@ -2,19 +2,23 @@ import { useTexture } from "@react-three/drei"
 import { Object3DNode, useFrame, Vector3 } from "@react-three/fiber"
 import { useRef, useState } from "react"
 import { Texture } from "three"
+import { textureMap } from "./texturemap"
 import { typeBlock } from "./type"
 
 export function BlockRender(props: {
     block: typeBlock
 }) {
-    const colorMap = useTexture(`textures/${props.block.name.split(":").splice(1).join(":")}.png`)
+    let pngname = `${props.block.name.split(":").splice(1).join(":")}`
+    if (!Object.hasOwn(textureMap, pngname))
+        pngname = "null"
+    const [colorMap, nullmap] = useTexture([`textures/${pngname}.png`, "textures/null.png"])
 
     return (
         <mesh
             position={[props.block.pos.x, props.block.pos.y, props.block.pos.z]}
         >
             <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial map={colorMap} />
+            <meshStandardMaterial map={colorMap ? colorMap : nullmap} />
         </mesh>
     )
 }
