db541ba366bab75cf4f4c483ac7f45c5d89fcb15
Brett <bretthoganpr@gmail.com>
2022-08-15 22:28:09 -0400

    BlockRender: Test display of bedrock

--- turtlesite/src/Block.tsx
+++ turtlesite/src/Block.tsx
@@ -1,16 +1,18 @@
-import { Object3DNode, useFrame, Vector3 } from "@react-three/fiber"
+import { Object3DNode, useFrame, useLoader, Vector3 } from "@react-three/fiber"
 import { useRef, useState } from "react"
+import { TextureLoader } from "three"
 import { typeBlock } from "./type"
 
 export function BlockRender(props: {
     block: typeBlock
 }) {
+    const colorMap = useLoader(TextureLoader, `textures/bedrock.png`)
     return (
         <mesh
             position={[props.block.pos.x, props.block.pos.y, props.block.pos.z]}
         >
             <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={'orange'} />
+            <meshStandardMaterial map={colorMap} />
         </mesh>
     )
 }
