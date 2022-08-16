98d9255cb8e41d143df79ba9c60910a0a58260ad
Brett <bretthoganpr@gmail.com>
2022-08-15 22:48:36 -0400

    BlockRender: Renders block textures based on name

--- turtlesite/src/Block.tsx
+++ turtlesite/src/Block.tsx
@@ -1,12 +1,13 @@
 import { Object3DNode, useFrame, useLoader, Vector3 } from "@react-three/fiber"
 import { useRef, useState } from "react"
-import { TextureLoader } from "three"
+import { Texture, TextureLoader } from "three"
 import { typeBlock } from "./type"
 
 export function BlockRender(props: {
     block: typeBlock
 }) {
-    const colorMap = useLoader(TextureLoader, `textures/bedrock.png`)
+    const colorMap = useLoader(TextureLoader, `textures/${props.block.name.split(":").splice(1).join(":")}.png`)
+
     return (
         <mesh
             position={[props.block.pos.x, props.block.pos.y, props.block.pos.z]}
